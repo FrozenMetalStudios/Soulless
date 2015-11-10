fa8e539f7b89452463f6f94a2db858c47360ea89
Pravjot Rai <pravjot.s.rai@gmail.com>
2015-11-09 23:12:53 -0800

    Game assets

---
+++ Project/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public int startingHealth = 100;                //Enemy health on start of game
+    public int currentHealth;                       //Enemies current health
+    public int scoreValue = 10;                     //The amount added to the players score when the enemy dies
+    //public AudioClip dealthClip;                    //Enemies death audio clip
+
+    private Animator anim;                          //Reference to enemy characters animator
+   // private AudioSource enemyAudio;                 //Reference to enemy characters audio source
+    private ParticleSystem hitParticles;            //Reference to the particle system that plays when enemy is damaged
+    private CapsuleCollider capsuleCollider;        //Reference to the enemies capsule collider
+    private bool isDead;                            //Check to see if the enemy has died
+
+	// Use this for initialization
+	void Awake () {
+        //Setting up the references
+        anim = GetComponent<Animator>();
+        //enemyAudio = GetComponent<AudioSource>();
+        hitParticles = GetComponent<ParticleSystem>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
+        //setting the current health when the enemy first spawns
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void TakeDamage(int amount, Vector2 hitPoint)
+    {
+        // If the enemy is dead...
+        if (isDead)
+            // ... no need to take damage so exit the function.
+            return;
+
+        // Play the hurt sound effect.
+        //enemyAudio.Play();
+
+        // Reduce the current health by the amount of damage sustained.
+        currentHealth -= amount;
+
+        // Set the position of the particle system to where the hit was sustained.
+        hitParticles.transform.position = hitPoint;
+
+        // And play the particles.
+        hitParticles.Play();
+
+        // If the current health is less than or equal to zero...
+        if (currentHealth <= 0)
+        {
+            // ... the enemy is dead.
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        // The enemy is dead.
+        isDead = true;
+
+        // Turn the collider into a trigger so shots can pass through it.
+        capsuleCollider.isTrigger = true;
+
+        // Tell the animator that the enemy is dead.
+        anim.SetTrigger("Dead");
+
+        // Change the audio clip of the audio source to the death clip and play it (this will stop the hurt clip playing).
+        //enemyAudio.clip = deathClip;
+       // enemyAudio.Play();
+    }
+}
---
+++ Project/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+    public int startingHealth = 100;                                //Starting health for player
+    public int currentHealth;                                       //Players current health 
+    public Slider healthSlider;                                     //Reference to the slider object
+    public Image damageImage;                                       //Reference to the image that will be flashed whne player takes damage
+    //public AudioClip dealthClip;                                    //The audio clip that plays when player dies
+    public float flashSpeed = 5f;                                   //The speed the death image will flash
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);         //The color of the death image
+
+    private Animator anim;                                          //Reference to the players animator
+    //private AudioSource playerAudio;
+    private PlayerController playerMovement;                        //Reference to the player movement controller so we can disable movement when player dies
+    private bool isDead;                                            //Players death flag
+    private bool isDamaged;                                         //Player is damaged flag
+
+	// Use this for initialization
+	void Awake () {
+        //Get necessary references
+        anim = GetComponent<Animator>();
+        //playerAudio = GetComponent<AudioSource>();
+        playerMovement = GetComponent<PlayerController>();
+
+        //Set the players health to the startingHealth when player spawns
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if character is damaged...
+        if (isDamaged)
+        {
+            //... flash the damage images
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            //... transition the color back to a clear background
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        isDamaged = false;
+	}
+
+    public void TakeDamage(int amount)
+    {
+        //set the damage flag so the damage image will flash
+        isDamaged = true;
+
+        //Reduce the current health of the player
+        currentHealth -= amount;
+
+        //Update the health slider with the value of the current health
+        healthSlider.value = currentHealth;
+
+        //Play Hurt audio clip
+        //playerAudio.Play();
+
+        //Check to see if the players health is less than 0 and the dealth flag hasn't been set yet...
+        if (currentHealth <= 0 && !isDead)
+        {
+            //... player should die
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        //Set the death flag so this function wont go into a loop
+        isDead = true;
+
+        // Tell the animator that the player is dead
+        anim.SetTrigger("Die");
+
+        //Set the player audio clip to the death clip and play it
+       //playerAudio.clip = dealthClip;
+        //playerAudio.Play();
+
+        //disable the players movement
+        playerMovement.enabled = false;
+    }
+}
