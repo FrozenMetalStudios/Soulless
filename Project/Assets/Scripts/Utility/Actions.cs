725907246990cdadfe1b58273245f7044dd8a566
Pravjot Rai <pravjot.s.rai@gmail.com>
2016-02-07 22:50:07 -0800

    added comments

--- Project/Assets/Scripts/Utility/Actions.cs
+++ Project/Assets/Scripts/Utility/Actions.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 namespace Assets.Scripts.Utility
 {
+    // CombatActions
+    //<summary>
+    // Contains static types for calling Combat related actions
+    //</summary>
     public static class CombatActions
     {
         public static string UpdateDamage = "UpdateDamage";
